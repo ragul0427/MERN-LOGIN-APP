d2787fab2336fd7203b8f776209c5852050b3390
ragul0427 <121870806+ragul0427@users.noreply.github.com>
2023-05-14 23:58:27 +0530

    Create Conver.js

---
+++ Helper folder/Helper folder/Conver.js
@@ -0,0 +1,29 @@
+src/helper/convert.js
+
+
+
+/** image onto base64 */
+
+export default function convertToBase64(file){
+
+    return new Promise((resolve, reject) => {
+
+        const fileReader = new FileReader();
+
+        fileReader.readAsDataURL(file);
+
+        fileReader.onload = () => {
+
+            resolve(fileReader.result)
+
+        }
+
+        fileReader.onerror = (error) => {
+
+            reject(error)
+
+        }
+
+    })
+
+}
